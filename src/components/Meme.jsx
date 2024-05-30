0b0526f95bb01e683dff2f9280af415d40571fdf
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-05-30 23:55:51 +0200

    Removed the import from the HTML and got the memes from the API using the 'fetch' method

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import memesData from "../memesData.js ";
 
 export default function Meme() {
   const [meme, setMeme] = React.useState({
@@ -8,12 +7,17 @@ export default function Meme() {
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
 
-  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+  const [allMemes, setAllMemes] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
 
   function getMemeImage() {
-    const memesArray = memesData.data.memes;
-    const randomNumber = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[randomNumber].url;
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url;
     setMeme((prevMeme) => ({
       ...prevMeme,
       randomImage: url,
@@ -55,7 +59,11 @@ export default function Meme() {
           Get a new meme image 🖼
         </button>
       </div>
-      <img src={meme.randomImage} className="meme--image" />
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 }
